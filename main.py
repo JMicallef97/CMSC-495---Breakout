e510a2c8ab8e801d51ea9060dbd7f754caeea307
JMicallef97 <151626899+JMicallef97@users.noreply.github.com>
2025-03-31 08:45:07 +0900

    Main.py housekeeping
    Documented main.py through commenting, and performed housekeeping (removed unnecessary comments and spacing)

--- main.py
+++ main.py
@@ -1,189 +1,105 @@
-"""
-
-Starting Template
-
-
-Once you have learned how to use classes, you can begin your program with this
-
-template.
-
-
-If Python and Arcade are installed, this example can be run from the command line with:
-
-python -m arcade.examples.starting_template
-
-"""
-
 import arcade
 
-
+# Constants
 WINDOW_WIDTH = 1280
-
 WINDOW_HEIGHT = 720
-
 WINDOW_TITLE = "Starting Template"
 
-
-
+# This class contains code used to manage user input, game logic and rendering,
+# and the game window.
 class GameView(arcade.View):
 
-    """
-
-    Main application class.
-
-
-    NOTE: Go ahead and delete the methods you don't need.
-
-    If you do need a method, delete the 'pass' and replace it
-
-    with your own code. Don't leave 'pass' in this program.
-
-    """
-
-
+    # Constructor for the game class
     def __init__(self):
-
         super().__init__()
-
-
         self.background_color = arcade.color.AMAZON
 
-
-        # If you have sprite lists, you should create them here,
-
-        # and set them to None
-
-
+    # Resets the game to its initial state
     def reset(self):
-
-        """Reset the game to the initial state."""
-
-        # Do changes needed to restart the game here if you want to support that
-
         pass
 
-
+    # Contains code to draw the game to the screen
     def on_draw(self):
 
-        """
-
-        Render the screen.
-
-        """
-
-
         # This command should happen before we start drawing. It will clear
-
         # the screen to the background color, and erase what we drew last frame.
-
         self.clear()
-
 
         # Call draw() on all your sprite lists below
 
 
+    # Contains code to update the game
     def on_update(self, delta_time):
 
         """
-
         All the logic to move, and the game logic goes here.
-
         Normally, you'll call update() on the sprite lists that
-
         need it.
-
         """
 
         pass
 
-
+    # Used to detect and handle key presses
     def on_key_press(self, key, key_modifiers):
-
         """
-
         Called whenever a key on the keyboard is pressed.
 
-
         For a full list of keys, see:
-
         https://api.arcade.academy/en/latest/arcade.key.html
-
         """
-
         pass
 
 
+    # Used to detect and handle key releases
     def on_key_release(self, key, key_modifiers):
-
         """
-
         Called whenever the user lets off a previously pressed key.
-
         """
-
         pass
 
 
+    # Used to detect and handle mouse motion
     def on_mouse_motion(self, x, y, delta_x, delta_y):
-
         """
-
         Called whenever the mouse moves.
-
         """
-
         pass
 
-
+    # Used to detect and handle mouse clicks
     def on_mouse_press(self, x, y, button, key_modifiers):
-
         """
-
         Called when the user presses a mouse button.
-
         """
-
         pass
 
-
+    # Used to detect and handle releasing mouse buttons
     def on_mouse_release(self, x, y, button, key_modifiers):
-
         """
-
         Called when a user releases a mouse button.
-
         """
-
         pass
 
 
 
+# Contains the entry point of the application/game, which is used to set up the game
+# program
 def main():
 
     """ Main function """
 
     # Create a window class. This is what actually shows up on screen
-
     window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
-
     # Create and setup the GameView
-
     game = GameView()
 
-
     # Show GameView on screen
-
     window.show_view(game)
 
-
-    # Start the arcade game loop
-
+    # Start the arcade game loop (main game loop)
     arcade.run()
 
-
-
-
+# Used to start the application
 if __name__ == "__main__":
 
     main()
